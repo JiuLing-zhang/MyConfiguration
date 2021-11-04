319657babc6c29326a2baa0b5d38eb32642638c4
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-11-04 21:45:31 +0800

    新增微软URL重定向工具

---
+++ MicrosoftLanguageRedirect.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         微软URL重定向工具
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  如果打开的连接是非中文，则自动跳转到中文版
+// @author       九零
+// @match        https://*.microsoft.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    let url = window.location.href;
+    var reg = new RegExp("(https://\\S*.microsoft.com)/(en-us|zh-hk|zh-tw)/", "ig");
+    let result = reg.exec(url);
+    if (result == null) {
+        return;
+    }
+
+    window.document.write("<div style='text-align:center;font-size: 30px;margin-top: 30px;color: #B6895A;'>" + "准备为您跳转到中文版" + "</div>");
+    //原始路径，类似https://docs.microsoft.com/en-us/
+    let srcPart = result[0];
+    //去掉语言部分的路径，类似https://docs.microsoft.com
+    let dstPart = result[1];
+
+    //清除原始地址的头部
+    url = url.replace(srcPart, "");
+
+    //拼装中文版地址并跳转
+    url = dstPart + "/zh-cn/" + url;
+
+    window.location.href = url;
+})();
